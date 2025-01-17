cd29b690a6f5b49d68d02848be742fe43e68889c
Siddharth Suri <siddharth2005suri@gmail.com>
2025-01-17 10:50:11 +0530

    changes to image

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,37 +1,38 @@
 import React, { useMemo } from "react";
 import { CartIcon } from "../assets/CartIcon";
-import { Routes , useNavigate } from "react-router-dom"
-export const TopBar = React.memo(()=> {
-
-    const Navigate = useNavigate()
-    function NavigateToCart(){
-        Navigate('/cart')
-        
+import { Routes, useNavigate } from "react-router-dom";
+import Amazonlogo from "../assets/Amazoncart.png";
+export const TopBar = React.memo(() => {
+    const Navigate = useNavigate();
+    function NavigateToCart() {
+        Navigate("/cart");
     }
-    function NavigateToHome(){
-        Navigate('/')
-        
+    function NavigateToHome() {
+        Navigate("/");
     }
     return (
-      <div>
-        <div className=" text-white	flex items-center justify-between  bg-gray-800 h-28 ">
-          <div
-            className="text-2xl font-bold pl-5 border-transparent cursor-pointer"
-            onClick={NavigateToHome}
-          >
-            Amazon.in
-          </div>
-          <div className="flex space-x-5 cursor-pointer">
-            <div className="">Hello User</div>
+        <div>
+            <div className=" text-white	flex items-center justify-between  bg-gray-800 h-28 ">
+                <div
+                    className="text-2xl font-bold pl-5 border-transparent cursor-pointer"
+                    onClick={NavigateToHome}
+                >
+                    <img
+                        src={Amazonlogo}
+                        className="translate-y-2 align-middle items-center h-12 w-44"
+                    ></img>
+                </div>
+                <div className="flex space-x-5 cursor-pointer">
+                    <div className="">Hello User</div>
 
-            <div
-              className="pt-0.5 pr-5 cursor-pointer"
-              onClick={NavigateToCart}
-            >
-              <CartIcon></CartIcon>
+                    <div
+                        className="pt-0.5 pr-5 cursor-pointer"
+                        onClick={NavigateToCart}
+                    >
+                        <CartIcon></CartIcon>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     );
-})
+});
